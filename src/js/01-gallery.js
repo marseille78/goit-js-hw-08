37b9747cb2ff26ebeadc5fb217157bcba020f713
RuslanBoiko <boyko.ruslan78@gmail.com>
2023-02-26 18:04:45 +0200

    Added Gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,54 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+document.addEventListener('DOMContentLoaded', () => {
+  (function() {
+    if (!document.querySelector('.gallery')) return;
+
+    new GallerySimpleLightbox('.gallery', galleryItems);
+  })();
+});
+
+class GallerySimpleLightbox {
+  constructor(selector, data) {
+    this.rootElement = document.querySelector(selector);
+    this.data = data;
+
+    this.init();
+  }
+
+  init() {
+    this.renderTree();
+    new SimpleLightbox('.gallery a', {
+      captions: true,
+      captionsData: 'alt',
+      captionDelay: 250
+    });
+  }
+
+  renderTree() {
+    this.data.forEach(item => {
+      this.rootElement.append(this.createItem(item));
+    })
+  }
+
+  createItem({description, original, preview}) {
+    const img = document.createElement('img');
+    img.classList.add('gallery__image');
+    img.src = preview;
+    img.alt = description;
+
+    const link = document.createElement('a');
+    link.classList.add('gallery__item');
+    link.href = original;
+
+    link.append(img);
+
+    return link;
+  }
+}
 
 console.log(galleryItems);
