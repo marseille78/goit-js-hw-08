e1243ac92416cf9372cccd40524af7bc066c49dd
RuslanBoiko <boyko.ruslan78@gmail.com>
2023-03-01 00:08:22 +0200

    Added videoplayer

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+document.addEventListener('DOMContentLoaded', () => {
+  (function() {
+    const iframe = document.querySelector('#vimeo-player');
+    const player = new Player(iframe);
+
+    player.on('timeupdate', throttle(saveTime, 1000));
+
+    function saveTime() {
+      player.getCurrentTime().then(function(sec) {
+        localStorage.setItem('videoplayer-current-time', sec);
+      }).catch(function(err) {
+        throw new Error(err);
+      });
+    }
+
+    player.on('play', () => {
+      player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+        .then(sec => {})
+        .catch(err => {
+          throw new Error(err);
+        });
+    });
+  })();
+});
